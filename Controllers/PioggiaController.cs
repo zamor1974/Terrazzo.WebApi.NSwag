4fda4ae344334ae66e0efa6a5c7ad640c8b84ade
Luigi Amoroso <luigi.amoroso@gmail.com>
2021-01-18 15:12:36 +0100

    s

---
+++ Controllers/PioggiaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Terrazzo.WebApi.NSwag.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PioggiaController : ControllerBase
+    {
+        [HttpGet]
+        public Int64 Get()
+        {
+            return 124;
+            //var databaseHelper = new DatabaseHelper();
+            //var pioggia = databaseHelper.GetPioggiaAdesso();
+            //return pioggia.Valore;
+        }
+    }
+}
---
+++ Pioggia.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Terrazzo.WebApi.NSwag
+{
+    public class Piogge
+    {
+        public List<Pioggia> Misurazioni { get; set; }
+
+        public Piogge()
+        {
+            Misurazioni = new List<Pioggia>();
+        }
+    }
+    public class Pioggia
+    {
+        public long Valore { get; set; }
+        public string Stato { get; set; }
+    }
+}
