646efaf127c38e12fc554e0127672ea663d5fc0b
Luigi Amoroso <luigi.amoroso@gmail.com>
2021-02-17 13:49:52 +0100

    d

--- Controllers/PioggiaController.cs
+++ Controllers/PioggiaController.cs
@@ -13,7 +13,7 @@ namespace Terrazzo.WebApi.NSwag.Controllers
         [HttpGet]
         public Int64 Get()
         {
-            return 1255;
+            return 125;
             //var databaseHelper = new DatabaseHelper();
             //var pioggia = databaseHelper.GetPioggiaAdesso();
             //return pioggia.Valore;
